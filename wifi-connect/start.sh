ae4a97d718c62770279cfbc6e08c85d08656a93a
majorz <zahari@resin.io>
2018-04-19 12:14:35 +0300

    Make WiFi Connect listen on port 45454

--- wifi-connect/start.sh
+++ wifi-connect/start.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-# Choose a condition for running WiFi Connect according to your use case:
-
-# 1. Is there a default gateway?
-# ip route | grep default
-
-# 2. Is there Internet connectivity?
-# nmcli -t g | grep full
-
-# 3. Is there Internet connectivity via a google ping?
-# wget --spider http://google.com 2>&1
-
-# 4. Is there an active WiFi connection?
-iwgetid -r
-
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
-    printf 'Starting WiFi Connect\n'
-    ./wifi-connect
-fi
+./wifi-connect --portal-listening-port 45454
 
 sleep infinity
