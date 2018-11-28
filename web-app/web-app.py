235574e944a5c18c15e3a2a8b7dce3986a6b415d
Zahari Petkov <zahari@balena.io>
2018-11-28 22:16:26 +0200

    Redirecting on captive portal hosts
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- web-app/web-app.py
+++ web-app/web-app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
@@ -10,6 +10,11 @@ def root():
 def custom():
     print(request.form.get('custom'))
     return jsonify({"success": True})
+
+@app.before_request
+def before_request():
+    if request.host != '192.168.42.1':
+        return redirect('http://192.168.42.1/')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
